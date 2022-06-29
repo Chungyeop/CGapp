73e742afa859b46bf3c3e803bc68df316741e21c
Chungyeop <yeop1438@naver.com>
2022-06-29 16:27:01 +0900

    06.29 Base Update

---
+++ app/src/main/java/com/ds/cgapp/GameKActivity.java
@@ -0,0 +1,14 @@
+package com.ds.cgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameKActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_kactivity);
+    }
+}
