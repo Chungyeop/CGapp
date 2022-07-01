d7d98daf8ec97aee612ab56f4b8073c70a241dc9
milttang <dntjr043@naver.com>
2022-07-01 15:51:04 +0900

    Life Cycle add

--- app/src/main/java/com/ds/cgApp/GameKActivity.java
+++ app/src/main/java/com/ds/cgApp/GameKActivity.java
@@ -11,4 +11,33 @@ public class GameKActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_kactivity);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
